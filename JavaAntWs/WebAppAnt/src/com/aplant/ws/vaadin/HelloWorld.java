bb252e97b18216517b5b4719c3349896cf651c35
mrozk <mrozk@mrozk-K53E>
2014-03-08 18:48:14 +0200

    addManyFetures

--- JavaAntWs/WebAppAnt/src/com/aplant/ws/vaadin/HelloWorld.java
+++ JavaAntWs/WebAppAnt/src/com/aplant/ws/vaadin/HelloWorld.java
@@ -6,9 +6,12 @@ import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.data.util.TextFileProperty;
+import com.vaadin.event.MouseEvents.ClickEvent;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
@@ -22,10 +25,32 @@ public class HelloWorld  extends UI  {
 	
 	@Override
     protected void init(VaadinRequest request) {
+		final Button button = new Button("Push it!");
+
+		button.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+				 button.setCaption("You pushed it!");
+				
+			}
+
+			
+		});
+		
+		
+		
 		HorizontalSplitPanel split = new HorizontalSplitPanel();
+		
+		
 		setContent(split);
 		split.addComponent(docList);
-		split.addComponent(docView);
+		
+		HorizontalLayout hl = new HorizontalLayout();
+		hl.addComponent(button);
+		hl.addComponent(docView);
+		
+		split.addComponent(hl);
 		
 		docList.addValueChangeListener(new ValueChangeListener() {
 			@Override
